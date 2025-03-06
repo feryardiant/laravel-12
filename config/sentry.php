a2c59313f923f04875717fe706ffa7dfc44ca924
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-07 02:00:22 +0700

    feat: initialize `sentry` laravel integration
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ config/sentry.php
@@ -0,0 +1,32 @@
+<?php
+
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+
+/**
+ * Sentry Laravel SDK configuration file.
+ *
+ * @see https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/
+ */
+return [
+
+    // @see https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+    'dsn' => env('SENTRY_DSN'),
+
+    // @see https://spotlightjs.com/
+    // 'spotlight' => env('SENTRY_SPOTLIGHT', false),
+
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#logger
+    // 'logger' => Sentry\Logger\DebugFileLogger::class, // By default this will log to `storage_path('logs/sentry.log')`
+
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#ignore-exceptions
+    'ignore_exceptions' => [
+        CommandNotFoundException::class
+    ],
+
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#ignore-transactions
+    'ignore_transactions' => [
+        // Ignore Laravel's default health URL
+        '/up',
+    ],
+
+];
