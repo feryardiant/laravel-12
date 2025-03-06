1be8102537a034d916743bd5c9de697208f574aa
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-07 06:03:33 +0700

    feat: enhance frontend dx
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ resources/client/env.d.ts
@@ -0,0 +1,4 @@
+interface ImportMetaEnv {
+  APP_ENV?: 'local' | 'testing' | 'staging' | 'production'
+  VITE_APP_NAME?: string
+}
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,24 +7,11 @@ import { ZiggyVue } from 'ziggy-js'
 import { initializeTheme } from './composables/useAppearance'
 import '../css/app.css'
 
-// Extend ImportMeta interface for Vite...
-declare module 'vite/client' {
-  interface ImportMetaEnv {
-    readonly VITE_APP_NAME: string
-    [key: string]: string | boolean | undefined
-  }
+const appName = import.meta.env.VITE_APP_NAME ?? 'Laravel'
 
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-    readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>
-  }
-}
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
-
-createInertiaApp({
+void createInertiaApp({
   title: title => `${title} - ${appName}`,
-  resolve: name => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+  resolve: async name => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
