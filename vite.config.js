f3ac450f570643a6b7de75a5b213339d4886064c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-07 03:27:58 +0700

    feat: configure git hooks with conventional commit and lint staged
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import laravel from 'laravel-vite-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'resources/client'),
+      '@': resolve(__dirname, 'resources/js'),
+      'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+    },
+  },
+
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
