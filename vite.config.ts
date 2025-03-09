28b7f5a0265f1ba982ac4f4552653b14f677e8c9
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-03-09 13:11:57 +0700

    feat(dev): reconfigure `vite` as I want it to be
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,51 @@
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import laravel from 'laravel-vite-plugin'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'resources/client'),
-      '@': resolve(__dirname, 'resources/js'),
-      'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', ['APP'])
+  const isDev = ['local', 'testing'].includes(env.APP_ENV)
+
+  return {
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, 'resources/client'),
+        '@': resolve(__dirname, 'resources/js'),
+        'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+      },
     },
-  },
 
-  plugins: [
-    laravel({
-      input: ['resources/client/app.ts', 'resources/css/app.css'],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
+    build: {
+      sourcemap: isDev,
+      reportCompressedSize: false,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          /**
+           * @see https://rollupjs.org/configuration-options/#output-manualchunks
+           */
+          manualChunks: (id) => {
+            if (id.includes('node_modules'))
+              return 'vendor'
+          },
+        },
+      },
+    },
+
+    define: {
+      'import.meta.env.APP_NAME': JSON.stringify(env.APP_NAME),
+      'import.meta.env.APP_LOCALE': JSON.stringify(env.APP_LOCALE),
+      'import.meta.env.APP_URL': JSON.stringify(env.APP_URL),
+      'import.meta.env.APP_ENV': JSON.stringify(env.APP_ENV),
+    },
+
+    plugins: [
+      laravel({
+        input: ['resources/client/app.ts', 'resources/css/app.css'],
+        refresh: true,
+      }),
+      tailwindcss(),
+    ],
+  }
 })
